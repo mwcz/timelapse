037c40c4d260a06dff4d6102cef65f7f6197aecd
mwcz <mwc@clayto.com>
2013-08-29 15:25:19 -0400

    initial commit

---
+++ timelapse.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SAVE_PATH='~/Pictures/timelapse/testing/'
+SAVE_EXT='.jpg'
+CAPTURE_CMD='fswebcam'
+
+function capture {
+    $CAPTURE_CMD $SAVE_PATH$($1)$SAVE_EXT
+}
+
+while true
+do
+
+    # take the picture
+    capture 'date +%s'
+
+    # wait
+    sleep 1s
+
+    # push the file to dropbox
+    #db.py put ~/Pictures/timelapse/testing/$(date +%s).jpg Pictures/timelapse/testing/$(date +%s).jpg
+
+    # wait for the file to show up in dropbox before continuing
+    #while [true]
+    #do
+        #foo=$(db.py search Pictures/timelapse/testing/$(date +%s).jpg)
+        #if [[ foo -eq 'true' ]]; then
+            #statements
+        #fi
+        #sleep 1s
+    #done
+
+    # delete the file
+    #rm ~/Pictures/timelapse/testing/$(date +%s).jpg
+
+done
+
